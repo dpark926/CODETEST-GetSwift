155683a284dad49d0a4a342b5ecc5211e2cf86b1
Daniel Park <dpeace926@gmail.com>
2017-10-07 00:04:35 -0400

    display drone and package data

--- getswift-codetest/src/containers/GetSwift.js
+++ getswift-codetest/src/containers/GetSwift.js
@@ -39,10 +39,52 @@ class GetSwift extends Component {
   }
 
   render = () => {
+    let warehouseDrone = 0
+    let droneData = this.state.droneData.map( drone => {
+      if (drone.packages.length === 0) {
+        warehouseDrone += 1
+      }
+      return (
+        <div className="drone">
+          <p>Drone ID: {drone.droneId}</p>
+          <p>It's Current Location:</p>
+          <p>- {drone.location.latitude}</p>
+          <p>- {drone.location.longitude}</p>
+          <p>Holding Package:</p>
+        </div>
+      )
+    })
+
+    let packageData = this.state.packageData.map( eachPackage => {
+      return (
+        <div className="package">
+          <p>Package ID: {eachPackage.packageId}</p>
+          <p>Destination:</p>
+          <p>- {eachPackage.destination.latitude}</p>
+          <p>- {eachPackage.destination.longitude}</p>
+          <p>Deadline: {eachPackage.deadline}</p>
+        </div>
+      )
+    })
+
     return (
       <div>
         <h1>GetSwift's Code Test</h1>
         <div className='button' onClick={this.logData}>SHOW DATA</div>
+
+        <div className="droneSection">
+          <h1>DRONE DATA</h1>
+          <p>There are {this.state.droneData.length} drones in total</p>
+          <p>There are {warehouseDrone} drones in the warehouse</p>
+          <p>There are {this.state.droneData.length - warehouseDrone} drones out</p>
+
+          {droneData}
+        </div>
+        <div className="packageSection">
+          <h1>PACKAGE DATA</h1>
+          There are {this.state.packageData.length} unassigned packages
+          {packageData}
+        </div>
       </div>
     )
   }
